35709d93755db8a671b01cfe9916f0b61f24aa85
Tornabene Giovanni <t.giovanni76@gmail.com>
2026-02-01 14:28:21 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -49,8 +49,6 @@
  */
 import AGB from './pages/AGB';
 import AccessibilityAudit from './pages/AccessibilityAudit';
-import AdminAnalytics from './pages/AdminAnalytics';
-import AdminAutoModeration from './pages/AdminAutoModeration';
 import AdminDashboard from './pages/AdminDashboard';
 import AdminDisputes from './pages/AdminDisputes';
 import AdminModeration from './pages/AdminModeration';
@@ -59,8 +57,6 @@ import AdminReports from './pages/AdminReports';
 import AdminSEO from './pages/AdminSEO';
 import AdminSettings from './pages/AdminSettings';
 import AdminTickets from './pages/AdminTickets';
-import Blog from './pages/Blog';
-import BlogPost from './pages/BlogPost';
 import Cart from './pages/Cart';
 import Category from './pages/Category';
 import CompleteProfile from './pages/CompleteProfile';
@@ -75,18 +71,14 @@ import Favorites from './pages/Favorites';
 import Home from './pages/Home';
 import Impressum from './pages/Impressum';
 import ListingDetail from './pages/ListingDetail';
-import ListingPerformance from './pages/ListingPerformance';
-import LoyaltyProgram from './pages/LoyaltyProgram';
 import ManageCategories from './pages/ManageCategories';
 import ManageUsers from './pages/ManageUsers';
 import Marketplace from './pages/Marketplace';
-import MarketplaceDashboard from './pages/MarketplaceDashboard';
 import Messages from './pages/Messages';
 import ModerateListings from './pages/ModerateListings';
 import MyOrders from './pages/MyOrders';
 import MyPurchases from './pages/MyPurchases';
 import MySales from './pages/MySales';
-import MySubscriptions from './pages/MySubscriptions';
 import NewListing from './pages/NewListing';
 import NotificationCenter from './pages/NotificationCenter';
 import NotificationSettings from './pages/NotificationSettings';
@@ -94,11 +86,8 @@ import Notifications from './pages/Notifications';
 import PayPalWebhook from './pages/PayPalWebhook';
 import PreLaunchChecklist from './pages/PreLaunchChecklist';
 import PrivacyPolicy from './pages/PrivacyPolicy';
-import PromoteListing from './pages/PromoteListing';
 import Recommendations from './pages/Recommendations';
-import ReferralDashboard from './pages/ReferralDashboard';
 import RejectedListings from './pages/RejectedListings';
-import SellerAITools from './pages/SellerAITools';
 import SellerDashboard from './pages/SellerDashboard';
 import SellerHub from './pages/SellerHub';
 import SystemCheckup from './pages/SystemCheckup';
@@ -107,7 +96,6 @@ import TermsOfService from './pages/TermsOfService';
 import Transactions from './pages/Transactions';
 import UserProfile from './pages/UserProfile';
 import UserSettings from './pages/UserSettings';
-import Werbung from './pages/Werbung';
 import Widerrufsrecht from './pages/Widerrufsrecht';
 import __Layout from './Layout.jsx';
 
@@ -115,8 +103,6 @@ import __Layout from './Layout.jsx';
 export const PAGES = {
     "AGB": AGB,
     "AccessibilityAudit": AccessibilityAudit,
-    "AdminAnalytics": AdminAnalytics,
-    "AdminAutoModeration": AdminAutoModeration,
     "AdminDashboard": AdminDashboard,
     "AdminDisputes": AdminDisputes,
     "AdminModeration": AdminModeration,
@@ -125,8 +111,6 @@ export const PAGES = {
     "AdminSEO": AdminSEO,
     "AdminSettings": AdminSettings,
     "AdminTickets": AdminTickets,
-    "Blog": Blog,
-    "BlogPost": BlogPost,
     "Cart": Cart,
     "Category": Category,
     "CompleteProfile": CompleteProfile,
@@ -141,18 +125,14 @@ export const PAGES = {
     "Home": Home,
     "Impressum": Impressum,
     "ListingDetail": ListingDetail,
-    "ListingPerformance": ListingPerformance,
-    "LoyaltyProgram": LoyaltyProgram,
     "ManageCategories": ManageCategories,
     "ManageUsers": ManageUsers,
     "Marketplace": Marketplace,
-    "MarketplaceDashboard": MarketplaceDashboard,
     "Messages": Messages,
     "ModerateListings": ModerateListings,
     "MyOrders": MyOrders,
     "MyPurchases": MyPurchases,
     "MySales": MySales,
-    "MySubscriptions": MySubscriptions,
     "NewListing": NewListing,
     "NotificationCenter": NotificationCenter,
     "NotificationSettings": NotificationSettings,
@@ -160,11 +140,8 @@ export const PAGES = {
     "PayPalWebhook": PayPalWebhook,
     "PreLaunchChecklist": PreLaunchChecklist,
     "PrivacyPolicy": PrivacyPolicy,
-    "PromoteListing": PromoteListing,
     "Recommendations": Recommendations,
-    "ReferralDashboard": ReferralDashboard,
     "RejectedListings": RejectedListings,
-    "SellerAITools": SellerAITools,
     "SellerDashboard": SellerDashboard,
     "SellerHub": SellerHub,
     "SystemCheckup": SystemCheckup,
@@ -173,12 +150,11 @@ export const PAGES = {
     "Transactions": Transactions,
     "UserProfile": UserProfile,
     "UserSettings": UserSettings,
-    "Werbung": Werbung,
     "Widerrufsrecht": Widerrufsrecht,
 }
 
 export const pagesConfig = {
-    mainPage: "MarketplaceDashboard",
+    mainPage: "AGB",
     Pages: PAGES,
     Layout: __Layout,
 };
