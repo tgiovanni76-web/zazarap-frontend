7c5ecaf932f789eaf0666329f5d77fa1e02b028c
Tornabene Giovanni <t.giovanni76@gmail.com>
2026-03-16 19:34:11 +0000

    File changes

--- src/pages/ListingDetail.jsx
+++ src/pages/ListingDetail.jsx
@@ -41,7 +41,7 @@ export default function ListingDetail() {
     queryKey: ['listing', listingId],
     queryFn: async () => {
       const listings = await base44.entities.Listing.filter({ id: listingId });
-      return listings[0];
+      return listings?.[0] ?? null; // react-query v5: never return undefined
     },
     enabled: !!listingId
   });
