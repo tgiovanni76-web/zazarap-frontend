fd8cd6db4e0ca3f96a27dc3d79dc1e1a5b8fb40b
Tornabene Giovanni <t.giovanni76@gmail.com>
2026-03-18 19:37:48 +0000

    File changes

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -72,6 +72,7 @@ export default function Category() {
                 </Link>
               </CardContent>
             </Card>
+            </React.Fragment>
           ))}
         </div>
       )}
