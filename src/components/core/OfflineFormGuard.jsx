8fde32cb25ec4b1ca0f92e5bd8f9a24e3569b79d
Tornabene Giovanni <t.giovanni76@gmail.com>
2026-03-18 18:30:51 +0000

    File changes

--- src/components/core/OfflineFormGuard.jsx
+++ src/components/core/OfflineFormGuard.jsx
@@ -43,7 +43,7 @@ export default function OfflineFormGuard() {
     };
   }, []);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const onSubmit = (e) => {
       try {
         if (typeof navigator === 'undefined') return;
